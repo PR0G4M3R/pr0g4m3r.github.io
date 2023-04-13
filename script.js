556f4727a7c66634807ae73991500d5c59f1dd75
PR0G4M3R <2020BOIGZ@gmail.com>
2023-04-12 17:17:56 -0700

    Update 2.3

--- script.js
+++ script.js
@@ -1,21 +1,11 @@
-function EasyPeasyParallax() {
-	scrollPos = $(this).scrollTop();
-	$('.p1').css({
-		'background-position' : '50% ' + (-scrollPos/4)+"px"
-	});
-  $('.p2').css({
-		'background-position' : '50% ' + (-scrollPos/8)+"px"
-	});
-  $('.p3').css({
-		'background-position' : '70% ' + (-scrollPos/16)+"px"
-	});
-	$('.parallax-text').css({
-		'margin-top': (scrollPos/2)+"px",
-		'opacity': 1-(scrollPos/230)
-	});
-}
-$(document).ready(function(){
-	$(window).scroll(function() {
-		EasyPeasyParallax();
-	});
+document.addEventListener('keydown', (e) => {
+  if (e.key.toLowerCase() === 's') {
+      scrollBy(0,35)
+  }
+});
+
+document.addEventListener('keydown', (e) => {
+  if (e.key.toLowerCase() === 'w') {
+      scrollBy(0,-35)
+  }
 });
