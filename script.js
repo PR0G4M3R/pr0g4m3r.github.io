2fbbd1c4d9748a16bd93277d9df2d1aca20ffc7e
PR0G4M3R <118217184+PR0G4M3R@users.noreply.github.com>
2023-04-14 14:50:22 -0700

    Update 7.3.4

--- script.js
+++ script.js
@@ -3,8 +3,6 @@ let alertMsg = ['Hello There!', 'Welcome!', 'Hi!'];
 window.alert(alertMsg[Math.floor(Math.random() * alertMsg.length)]);
 
 
-
-
 document.addEventListener('keydown', (e) => {
   if (e.key.toLowerCase() === 's') {
       scrollBy(0,35)
